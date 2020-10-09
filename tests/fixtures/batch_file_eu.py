962de2b349234b21b20d651d496485f6ea146544
alfo1995 <damelioalfonso@hotmail.com>
2020-10-09 17:29:41 +0200

    feat(test): added new test for the new apis

--- tests/fixtures/batch_file_eu.py
+++ tests/fixtures/batch_file_eu.py
@@ -20,51 +20,52 @@ from typing import Optional
 import pytest
 
 from immuni_common.models.enums import TransmissionRiskLevel
-from immuni_common.models.mongoengine.batch_file import BatchFile
+from immuni_common.models.mongoengine.batch_file_eu import BatchFileEu
 from immuni_common.models.mongoengine.temporary_exposure_key import TemporaryExposureKey
 
 
-def generate_random_key_data(length: int = 128) -> str:
+def generate_random_key_data_eu(length: int = 128) -> str:
     letters = string.ascii_lowercase + string.ascii_uppercase
     return "".join(random.choice(letters) for _ in range(length))
 
 
-def generate_random_batch(
-    index: int, num_keys: int, period_start: datetime, period_end: datetime
+def generate_random_batch_eu(
+    index: int, num_keys: int, period_start: datetime, period_end: datetime, origin: str
 ) -> None:
     rsn = int(datetime.utcnow().timestamp() / 600)
 
     keys = [
         TemporaryExposureKey(
-            key_data=generate_random_key_data(),
+            key_data=generate_random_key_data_eu(),
             transmission_risk_level=random.choice([tr for tr in TransmissionRiskLevel]),
             rolling_start_number=rsn,
         )
         for _ in range(num_keys)
     ]
-    BatchFile(
-        index=index, keys=keys, period_start=period_start, period_end=period_end, origin="IT"
+    BatchFileEu(
+        index=index, keys=keys, period_start=period_start, period_end=period_end, origin=origin
     ).save()
 
 
-def create_random_batches(
+def create_random_batches_eu(
     num_batches: int, key_per_batch: int = 10, end_date: Optional[datetime] = None
 ) -> None:
     if end_date is None:
         end_date = datetime.utcnow()
     start_date = end_date - timedelta(days=num_batches)
     for index in range(num_batches):
-        generate_random_batch(
+        generate_random_batch_eu(
             index=index,
             num_keys=key_per_batch,
             period_start=start_date + timedelta(days=index),
             period_end=start_date + timedelta(days=index + 1),
+            origin="DK",
         )
 
 
 @pytest.fixture()
-def batch_file() -> BatchFile:
-    batch = BatchFile(
+def batch_file_eu() -> BatchFileEu:
+    batch_eu = BatchFileEu(
         index=1,
         keys=[
             TemporaryExposureKey(
@@ -87,8 +88,8 @@ def batch_file() -> BatchFile:
         period_end=datetime.utcnow(),
         sub_batch_count=2,
         sub_batch_index=1,
-        origin="IT",
+        origin="DK",
         client_content=b"this_is_a_zip_file",
     )
-    batch.save()
-    return batch
+    batch_eu.save()
+    return batch_eu
